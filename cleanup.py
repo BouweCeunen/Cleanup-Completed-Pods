45ec3a49b7df44e974b6f7284becb700f51f583e
Bouwe Ceunen <bouwe.ceunen@rombit.be>
2019-05-17 10:56:38 +0200

    push_all

--- cleanup.py
+++ cleanup.py
@@ -7,6 +7,8 @@ config.load_incluster_config()
 
 kubernetescorev1 = client.CoreV1Api()
 
-for pod in kubernetescorev1.list_namespaced_pod(NAMESPACE, field_selector='status.phase=Succeeded').items:
+succeeded_pods = kubernetescorev1.list_namespaced_pod(NAMESPACE, field_selector='status.phase=Succeeded').items
+failed_pods = kubernetescorev1.list_namespaced_pod(NAMESPACE, field_selector='status.phase=Failed').items
+for pod in succeeded_pods + failed_pods:
     print('Deleted succeeded pod %s in namespace %s' % (pod.metadata.name, NAMESPACE))
     kubernetescorev1.delete_namespaced_pod(pod.metadata.name,NAMESPACE)
